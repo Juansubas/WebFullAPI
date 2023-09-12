af528c7abb4d43f3a26cb8d706841192890631ae
juansubas <juansubas@gmail.com>
2023-09-11 23:45:36 -0500

    cREANDO dtos, mODELOS, eNUMS, Automapper y Configurando en appsetings.

---
+++ MagivVilla_Utility/DS.cs
@@ -0,0 +1,13 @@
+﻿namespace MagivVilla_Utility
+{
+    public static class DS
+    {
+        public enum APITipo
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
