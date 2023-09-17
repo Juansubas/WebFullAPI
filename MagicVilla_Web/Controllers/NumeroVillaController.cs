39c6e89d3d5c6304de7e03c27ef7c59f4f4e113b
juan.paez <juan.paez@dms.ms>
2023-09-17 00:32:32 -0500

    Creando ViewModel, Vista para crear NumeroVilla y Su metodo para alimentar dicha vista.

--- MagicVilla_Web/Controllers/NumeroVillaController.cs
+++ MagicVilla_Web/Controllers/NumeroVillaController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Models.ViewModel;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers
@@ -10,10 +12,12 @@ namespace MagicVilla_Web.Controllers
     public class NumeroVillaController : Controller
     {
         private readonly INumeroVillaService _numeroVillaService;
+        private readonly IVillaService _villaService;
         private readonly IMapper _mapper;
-        public NumeroVillaController(INumeroVillaService numeroVillaService, IMapper mapper) 
+        public NumeroVillaController(INumeroVillaService numeroVillaService, IMapper mapper, IVillaService villaService) 
         { 
             _numeroVillaService = numeroVillaService;
+            _villaService = villaService;
             _mapper = mapper;
         }
 
@@ -29,6 +33,24 @@ namespace MagicVilla_Web.Controllers
             }
 
             return View(numeroVillaList);
+        }
+
+        public async Task<IActionResult> CrearNumeroVilla()
+        {
+            NumeroVillaViewModel numeroVillaVM = new();
+
+            var response = await _villaService.ObtenerTodos<APIResponse>();
+            if(response != null && response.IsSuccess )
+            {
+                numeroVillaVM.villaList = JsonConvert.DeserializeObject<List<VillaDto>>(Convert.ToString(response.Resultado)).
+                                          Select(v => new SelectListItem
+                                          {
+                                              Text = v.Name,
+                                              Value = v.Id.ToString(),
+                                          });
+            }
+
+            return View(numeroVillaVM);
         }
     }
 }
