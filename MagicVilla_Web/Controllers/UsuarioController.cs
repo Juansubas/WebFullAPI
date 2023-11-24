3434fac765d24a510692783ba9949acad12e219a
juan.paez <juan.paez@dms.ms>
2023-11-24 16:27:33 -0500

    Metodos de accion para conectarse a la vistar

---
+++ MagicVilla_Web/Controllers/UsuarioController.cs
@@ -0,0 +1,52 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioService _usuarioService;
+        public UsuarioController(IUsuarioService usuarioService)
+        {   
+            _usuarioService = usuarioService;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginRequestDto modelo) 
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registrar(RegistroRequestDto modelo)
+        {
+            var response = await _usuarioService.Registrar<APIResponse>(modelo);
+            if(response != null && response.IsSuccess)
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoDenegado() 
+        {
+            return View();
+        }
+    }
+}
